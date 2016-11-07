adaabf66ae639ff28b40ab0461e2d68751c93ff9
Charles Chatwin <cmc699@nau.edu>
2016-11-07 13:26:59 -0700

    Created C files for userInfo and UserCreate and LoginActivity. Modified layouts for those as well.

---
+++ app/src/main/java/com/urch/chatwin/ibuy/UserCreate.java
@@ -0,0 +1,36 @@
+package com.urch.chatwin.ibuy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+/**
+ * Created by Charles Chatwin on 11/7/2016.
+ */
+public class UserCreate extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_user_create);
+
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.custom_title);
+
+    }
+
+    public void createUser() {
+        //TODO Create the user
+    }
+
+    public void backToLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
+}
---
+++ app/src/main/java/com/urch/chatwin/ibuy/userInfo.java
@@ -0,0 +1,50 @@
+package com.urch.chatwin.ibuy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+/**
+ * Created by Charles Chatwin on 11/7/2016.
+ */
+public class userInfo extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_user_info);
+
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.custom_title);
+
+    }
+
+    public void showList(View view) {
+        Intent intent = new Intent(this, List.class);
+        startActivity(intent);
+    }
+
+    public void addItem(View view) {
+        Intent intent = new Intent(this, AddList.class);
+        startActivity(intent);
+    }
+
+    public void logOut(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+    /*
+    public void addRoute(View view) {
+        Intent intent = new Intent(this, AddRoute.class);
+        startActivity(intent);
+    }
+    */
+}
+
+
+
