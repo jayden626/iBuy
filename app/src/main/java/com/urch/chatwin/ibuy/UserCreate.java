de9d6b5322751a71a5b406abfb2b3f24f8e370a6
Charles Chatwin <cmc699@nau.edu>
2016-11-14 23:34:43 -0700

    Re-created the LoginActivity (somehow it was deleted). Edited the UserCreate to now create a user using the DB.addUser. LoginActivity should also now be able to login to a user within the DB.getAllUsers.

--- app/src/main/java/com/urch/chatwin/ibuy/UserCreate.java
+++ app/src/main/java/com/urch/chatwin/ibuy/UserCreate.java
@@ -6,11 +6,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Charles Chatwin on 11/7/2016.
  */
 public class UserCreate extends AppCompatActivity {
+
+    DB_Handler db;
+    TextView password;
+    TextView username;
+    ArrayList<User> userList;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +31,30 @@ public class UserCreate extends AppCompatActivity {
 
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.custom_title);
 
+        //creating/assigning DB and textview's
+        db = new DB_Handler(this.getApplicationContext());
+        username = (TextView) findViewById(R.id.new_user_name);
+        password = (TextView) findViewById(R.id.new_password);
+        userList = db.getAllUsers();
     }
 
     public void createUser() {
-        //TODO Create the user
+
+        if (userList.contains(username.getText())) {
+            username.setText("Username Already Exists; try again!");
+        }
+        else {
+            db.addUser((String)username.getText()); //TODO if addUser is made to include password: add '(String)password.getText()'
+
+            //clearing the input to let user know it was created
+            username.setText("");
+            password.setText("");
+        }
+
     }
 
     public void backToLogin(View view) {
-        Intent intent = new Intent(this, MainMenu.class);//LoginActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);//LoginActivity.class);
         startActivity(intent);
     }
 }
